6f405ad040c9f02b344068342bb7d2dfe7701578
Ryan Skinner <ryan.skinner1193@gmail.com>
2021-06-29 08:46:34 -0400

    testin gh run

--- client/src/App.js
+++ client/src/App.js
@@ -18,7 +18,7 @@ function App() {
             target="_blank"
             rel="noopener noreferrer"
           >
-            Learn React
+            testing 1234
           </a>
           <Link to="/">Home</Link>
           <Link to="/otherpage">Other Page</Link>
